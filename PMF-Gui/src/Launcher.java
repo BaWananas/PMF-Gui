8f0ddf1b771f36b90b5923d1dbb644168873d1c9
Wananas <adadas54@gmail.com>
2018-11-27 15:12:55 +0100

    Creating Nathan branch

--- PMF-Gui/src/Launcher.java
+++ PMF-Gui/src/Launcher.java
@@ -1,12 +1,21 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
+import model.ModelFacade;
+import view.ViewFacade;
 
 public class Launcher extends Application{
-
+	
+	private ModelFacade model;
+	private ViewFacade view;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		
+		this.model = new ModelFacade();
+		this.view = new ViewFacade();
 	}
 
 	
